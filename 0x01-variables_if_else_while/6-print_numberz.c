70a2c0e3cd079ec71e14746e3417c8ffef49f4bb
Sarah Hassan <sarraree5@gmail.com>
2023-03-27 02:51:17 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,7 +11,10 @@ int main(void)
 	int num;
 
 	for (num = 0; num < 10; num++)
-		putchar(num + '0'); //another solution putchar(num + 48);
+		putchar(num + '0'); 
+/**
+another solution putchar(num + 48);
+*/
 
 	putchar('\n');
 
