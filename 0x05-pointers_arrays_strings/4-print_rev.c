3e13c3bb3ba91dbed2fd8534c4cf3e9fd4b3ff69
Sarah Hassan <sarraree5@gmail.com>
2023-03-27 17:41:24 -0700

    tasks

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_rev -> printing a string in reverse
+* @s: the string to be printed in rev
+*/
+
+void print_rev(char *s)
+{
+	int i; len;
+
+        len = _strlen(s);
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
