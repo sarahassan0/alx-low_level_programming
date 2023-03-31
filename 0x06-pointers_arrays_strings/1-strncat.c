dc8ff631adf9f8ea5c4e8a950ea7a7d0b97ab4fb
Sarah Hassan <sarraree5@gmail.com>
2023-03-30 19:44:54 -0700

    tasks

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,10 +13,10 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	while ( dest[i] != '\0' )
+	while (dest[i] != '\0')
 	{
-            i++;
-        }
+		i++;
+	}
 	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
 		dest[i + j] = src[j];
