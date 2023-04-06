2ff74112b3f520d584d67b9adbd5184fe2d1e892
Sarah Hassan <sarraree5@gmail.com>
2023-04-06 14:49:11 -0700

    tasks

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include main.h
+
+/**
+  * _print_rev_recursion - print a string in reverse
+  * @s: string
+  */
+
+void print_rev_recursion(char *s)
+{
+	if (!*s == '\0')
+	{
+		print_rev_recursion(s + 1)
+		_putchar(*s)
+	}
+}
