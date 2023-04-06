026ea09b1fabc25d863c044bfd5d8cd0b861570e
Sarah Hassan <sarraree5@gmail.com>
2023-04-06 16:31:05 -0700

    tasks

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,15 +9,13 @@
 int factorial(int n)
 {
 
-	int fact;
-
 	if (n < 0)
 	{
-		reurn(-1);
+		return (-1);
 	}
 	else if (n == 0)
 	{
-		retrun(1);
+		return (1);
 	}
 	return (n * factorial(n - 1));
 }
