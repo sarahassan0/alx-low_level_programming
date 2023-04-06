0b0d428db62d20f76b661d6cf2c96787a2bf3c72
Sarah Hassan <sarraree5@gmail.com>
2023-04-06 15:58:52 -0700

    tasks

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
-	static int len = 1;
+	int len = 0;
 
 	if (*s != '\0')
 	{
 		len++;
-		_strlen_recursion(s + 1);
+		len += _strlen_recursion(s + 1);
 	}
 	return (len - 1);
 }
