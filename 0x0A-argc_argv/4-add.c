32b106cf6b28dad57a8173786b1debc179cb9d01
Sarah Hassan <sarraree5@gmail.com>
2023-04-09 22:47:42 -0700

    tasks

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+	int i, x, sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i <= argc; i++)
+	{
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		x += atoi(argv[i]);
+	}
+	printf("%d\n", x);
+	return (0);
+}
