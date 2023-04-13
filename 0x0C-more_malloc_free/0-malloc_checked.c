fa9539efae69b3c417a83388f2cbff628cad38bd
Sarah Hassan <sarraree5@gmail.com>
2023-04-13 05:29:53 -0700

    tasks

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ *         or exit with status code 98 on failure
+ */
+
+void *malloc_checked(unsigned int b)
+{
+    char *ptr;
+
+    ptr = malloc(b);
+
+    if (ptr == NULL)
+    {
+        exit(98);
+    }
+
+    return (ptr);
+}
+
