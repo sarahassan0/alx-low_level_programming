1b041062013ee9be5c6676cadb3bf6cae3769984
Sarah Hassan <sarraree5@gmail.com>
2023-04-14 04:04:07 +0200

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -27,7 +27,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	memset(ptr, 0, nmemb * size);
+	for (i = 0; i < (nmemb * size); i++)
+		ptr[i] = 0;
 	return (ptr);
 }
 
