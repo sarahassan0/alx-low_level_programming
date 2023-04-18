cb9275d3d969d483ddd829f34855ac913999a0ff
Sarah Hassan <sarraree5@gmail.com>
2023-04-18 11:35:08 -0700

    tasks

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 
 	if (d == NULL)
 	{
-		return (0);
+		return;
 	}
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %.6f\n", d->age ? d->age : 0);
