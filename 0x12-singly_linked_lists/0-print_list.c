8b3365a326cc26823f69dea10b91b43d0f1ed6e7
Sarah Hassan <sarraree5@gmail.com>
2023-04-27 17:05:21 -0700

    tasks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		printf("[%d] %s\n", h->len, h->str != NULL ? h->str : "(nil)");
-		count++;
+		nodes_num;
 		h = h->next;
 	}
 	return (nodes_num);
