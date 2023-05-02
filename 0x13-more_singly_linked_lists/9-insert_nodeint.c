07da6cc5fc9300a23e431a39bf18500927e81f8b
Sarah Hassan <sarraree5@gmail.com>
2023-05-02 16:30:09 -0700

    tasks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (new);
 		}
 	}
-	while (idx - 1)
+	while ((idx - 1))
 	{
 		tmp = tmp->next;
 		if (tmp == NULL)
