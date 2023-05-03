e9c7323f829b81e2cd482352c8288615a4abf5a2
Sarah Hassan <sarraree5@gmail.com>
2023-05-03 10:55:03 -0700

    2

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * Apply the constructor attribute to first() so that it
+ *   is executed before main()
+ */
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - implementation of first to execute before main
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
+}
