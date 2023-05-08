81aa3d2fc5120ace3733ae38625bef95ea239a20
sarah <sarraree5@gmail.com>
2023-05-09 01:05:28 +0300

    tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd_O = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd_O = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
 
 	if (fd_O == -1)
 	{
@@ -38,5 +38,5 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	close(fd_O);
-	return (fd_W);
+	return (1);
 }
