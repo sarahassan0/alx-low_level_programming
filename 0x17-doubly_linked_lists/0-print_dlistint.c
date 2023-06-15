6b1df3b5b036b3d5adbc74038f3d111ec5992326
sarah hassan <sarraree5.gmail.com>
2023-06-16 01:45:33 +0300

    Update - task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,11 +9,11 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes_count;
+	size_t nodes_count = 0;
 
 	while (h != NULL)
 	{
-		printf("%d", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		nodes_count++;
 	}
