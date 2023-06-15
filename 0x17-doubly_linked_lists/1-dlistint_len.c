3eb17ac8ab5ab3fb0879442daa76e70187b2a013
sarah hassan <sarraree5.gmail.com>
2023-06-16 02:04:49 +0300

    Update - task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,12 +9,12 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t nodes_count = 0;
+	size_t nodes_count = 0;
 
-    while (h != NULL)
-    {
-        h = h->next;
-        nodes_count++;
-    }
-    return (nodes_count);
+	while (h != NULL)
+	{
+		h = h->next;
+		nodes_count++;
+	}
+	return (nodes_count);
 }
