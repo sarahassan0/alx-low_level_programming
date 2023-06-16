864e8535fd8120351ac83d4223ca201264c24c90
sarah hassan <sarraree5.gmail.com>
2023-06-16 09:31:32 +0300

    Update - task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,12 +11,13 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
+	dlistint_t *node_to_insert_after;
 
 	if (h == NULL)
 		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	dlistint_t *node_to_insert_after = get_dnodeint_at_index(*h, idx - 1);
+	node_to_insert_after = get_dnodeint_at_index(*h, idx - 1);
 
 	if (node_to_insert_after->next == NULL)
 		return (add_dnodeint_end(h, n));
