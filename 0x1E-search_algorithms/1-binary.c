aa53d45cb1639f6fafb17f7bfe089f4e5cc23e87
sarahassan0 <sararee5@gmail.com>
2023-10-11 23:14:21 +0300

    remove additional space

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,7 +13,7 @@ int binary_search(int *array, size_t size, int value)
 {
 	int low = 0;
 	int high = size - 1;
-	int mid = size - 1 / 2;
+	int mid;
 
 	if (!array || !size || !value)
 		return (-1);
