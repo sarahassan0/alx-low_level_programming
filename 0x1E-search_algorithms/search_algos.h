618ce4381228ea79b1c822870076ab78fe4bf70e
sarahassan0 <sararee5@gmail.com>
2023-10-11 23:06:31 +0300

    task 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,4 +4,7 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print(int *array, int low, int high);
+
 #endif
